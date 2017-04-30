8c39eae6da8c23737675a242491d1c80d57f41ed
Yebin Lee <leeyebin007@gmail.com>
2017-05-01 00:18:20 +0900

    Sum of the first nth term of Series

--- Sum of the first nth term of Series/MySolution.java
+++ Sum of the first nth term of Series/MySolution.java
@@ -1,20 +1,18 @@
 import java.text.DecimalFormat;
 
 public class NthSeries {
-	
+
 	public static String seriesSum(int n) {
-		DecimalFormat form = new DecimalFormat("#.##");
-    float sum = 0;
-    
-		for(int i=1; i<=n; i++){
-			sum += 1.0/(1+3*(i-1));
+		float sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			sum += 1.0 / (1 + 3 * (i - 1));
 		}
-    if(n==0){
-      sum = 0;
-    }
-		
+		if (n == 0) {
+			sum = 0;
+		}
+
 		return String.format("%.2f", sum);
-    
-    
+
 	}
 }
