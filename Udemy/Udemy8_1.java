77cc1c0636e06a63330d517df2392e792108ca3a
Yebin Lee <leeyebin007@gmail.com>
2017-06-10 23:11:20 +0900

    Udemy

--- Udemy/Udemy8_1.java
+++ Udemy/Udemy8_1.java
@@ -1,31 +1,32 @@
 package codewars.udemy;
-public class Udemy5_3{
-	public static void main(String[] args){
-    	MyArrayStack s = new MyArrayStack();
-	s.push(1);
-	s.pop();
+
+public class Udemy8_1 {
+	public static void main(String[] args) {
+		MyArrayStack s = new MyArrayStack();
+		s.push(1);
+		s.pop();
 	}
 }
 
-class MyArrayStack{
+class MyArrayStack {
 	private int[] data = new int[100];
 	private int topIndex = -1;
-  
-	public void push(int i){
-		if(data.length==topIndex-1){
-		int[] oldData = data;
-		data = new int[data.length*2];
-		System.arraycopy(oldData, 0, data, 0, oldData.length);
+
+	public void push(int i) {
+		if (data.length == topIndex - 1) {
+			int[] oldData = data;
+			data = new int[data.length * 2];
+			System.arraycopy(oldData, 0, data, 0, oldData.length);
 		}
 
 		topIndex++;
 		data[topIndex] = i;
 	}
 
-	public int pop(){
-		if(topIndex<0){
+	public int pop() {
+		if (topIndex < 0) {
 			throw new RuntimeException("Nothing to pop");
 		}
-	return 0;
+		return 0;
 	}
 }
