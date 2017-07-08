fc4bdaad75eb6a23e4fcc81941bd02e4cf75c56a
Yebin Lee <leeyebin007@gmail.com>
2017-07-08 23:09:13 +0900

    Udemy

--- Udemy/Udemy10_4.java
+++ Udemy/Udemy10_4.java
@@ -2,8 +2,18 @@ package Udemy.Udemy10_4;
 
 public class TreeHandler {
 	public static boolean isBST(Node root) {
-		return isBstRec(root, Integer.MIN_VALUE, Integer.MAX_VALUE)
+		return isBstRec(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 	
-	pci
+	private static boolean isBstRec(Node node, int min, int max){
+		if(node==null){
+			return true;
+		}
+		if(node.data <= min || node.data > max){
+			return false;
+		}
+		boolean leftBst = isBstRec(node.left, min, node.data);
+		boolean rightBst = isBstRec(node.right, node.data, max);
+		return leftBst && rightBst;
+	}
 }
