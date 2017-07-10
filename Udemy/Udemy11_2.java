cb1d52736d885ed34cca9b1a57c2425da6c3f451
leeyebin <leeyebin@gmail.com>
2017-07-10 23:08:28 +0900

    Udemy

--- Udemy/Udemy11_2.java
+++ Udemy/Udemy11_2.java
@@ -2,26 +2,26 @@ import java.util.PriorityQueue;
 
 public class Top10 {
 	private PriorityQueue<Integer> q = new PriorityQueue<Integer>();
-	
+
 	public void read(int n){
 		if(q.size()<10){
 			q.offer(n);
 		}else{
-			int tenth = q.peek(); //Å¥¿¡¼­ °¡Àå ÀÛÀº ¼ö
+			int tenth = q.peek(); //íì˜ ê°€ìž¥ ìž‘ì€ ê°’
 			if(n>tenth){
 				q.poll();
 				q.offer(n);
 			}
 		}
 	}
-	
+
 	public int[] getTop10(){
 		int[] ret = new int[10];
 		for(int i=q.size()-1; i>=0; i--){
 			ret[i] = q.poll();
 		}
 		return ret;
-		
+
 	}
 
 }
