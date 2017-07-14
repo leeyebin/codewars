db6243d386e06d35972e41beba2e6e4a3a399336
Yebin Lee <leeyebin007@gmail.com>
2017-07-14 23:36:00 +0900

    ETC

---
+++ ETC/firstLastDate.js
@@ -0,0 +1,13 @@
+//년,월을 함수에 넣으면 해당 년월의 처음날짜와 마지막 날짜를 구한다.
+function fn_firstDate(yyyy, mm){
+	var firstDate = new Date(yyyy, mm-1, 1);
+	var firstMm = firstDate.getMonth()+1;
+	var firstDd = firstDate.getDate();
+	return firstDate.getFullYear() + '' + (firstMm < 10 ? '0'+ firstMm : firstMm) + '' + (firstDd < 10 ? '0'+ firstDd : firstDd);
+}
+function fn_lastDate(yyyy, mm){
+	var lastDate = new Date(yyyy, mm, 0);
+	var lastMm = lastDate.getMonth()+1;
+	var lastDd = lastDate.getDate();
+	return lastDate.getFullYear() + '' + (lastMm < 10 ? '0'+ lastMm : lastMm) + '' + (lastDd < 10 ? '0'+ lastDd : lastDd);
+}
