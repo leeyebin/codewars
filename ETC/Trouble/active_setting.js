09eafecd60a7c2f987e8747b4c8aa9e7033d6c63
leeyebin <leeyebin007@gmail.com>
2017-07-18 22:40:03 +0900

    active_setting.js

---
+++ ETC/Trouble/active_setting.js
@@ -0,0 +1,6 @@
+	//달력 클릭시 active
+	//.calender_box내의 .date_box내의 div들중에서 click이벤트 발생시 active를 없애고 클릭한 엘레먼트를 active상태로 바꾼다.
+	$('.calender_box .date_box > div').on('click', function(){
+		$('.calender_box .date_box > div').removeClass('active');
+		$(this).addClass('active');
+	});
