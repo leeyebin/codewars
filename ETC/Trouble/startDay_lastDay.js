df26169c33e82586aaf2f242070c849d5dafd71a
leeyebin <leeyebin007@gmail.com>
2017-07-19 22:35:16 +0900

    startDay_lastDay.js

---
+++ ETC/Trouble/startDay_lastDay.js
@@ -0,0 +1,12 @@
+//기준년월일이 들어왔을 경우 몇일 이후를 구하는 방법
+function fn_roomSummary(yyyymmdd, i){
+	var startday = yyyymmdd;
+	var lastdate = new Date(startday.substr(0,4), startday.substr(4,2), startday.substr(6,2));
+	var tempDd = lastdate.getDate();
+	var lastDd = lastdate.setDate(tempDd + i);//몇일 후인지
+	var lastMm = lastdate.getMonth();
+	var lastDd = lastdate.getDate();
+	var lastday = lastdate.getFullYear() + '' + (lastMm < 10 ? '0'+ lastMm : lastMm) + '' + (lastDd < 10 ? '0'+ lastDd : lastDd);
+	
+	return lastday
+}
